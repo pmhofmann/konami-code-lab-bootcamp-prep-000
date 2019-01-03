c074478badf49f0b0b000dd6e21ba8aa3b75f805
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2019-01-03 09:31:55 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,12 +13,12 @@ const codes = [
 
 
 function init() {
-  let idx = 0
+  let index = 0
 
   document.body.addEventListener("keydown", (e) => {
     const key = e.key
     
-    idx = (codes[idx] === key) ? ++idx : 0
+    index = (codes[index] === key) ? ++index : 0
     
     if (idx === codes.length) {
       window.alert("Hurray!");
