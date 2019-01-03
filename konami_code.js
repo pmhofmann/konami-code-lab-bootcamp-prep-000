3450ab442f81839cbfac5c54caf38219121e9f59
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2019-01-03 09:30:13 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,17 +1,15 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
-
 function init() {
-  let index = 0;
-  
+  let idx = 0
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+    
+    idx = (codes[idx] === key) ? ++idx : 0
+    
+    if (idx === codes.length) {
+      window.alert("Hurray!");
+      idx = 0
+    }
+    
+  });
 }
