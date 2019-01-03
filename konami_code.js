2af6ca14b782e8c9d972592ee1b21b29b3e67c47
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2019-01-03 09:43:00 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,15 +15,16 @@ const codes = [
 function init() {
   let index = 0;
 
-  document.body.addEventListener("keydown", (e) => {
-    const key = e.key;
-    
-    index = (codes[index] === key) ? ++index : 0;
-    
-    if (index === codes.length) {
-      alert("Hurray!");
-      index = 0;
-    }
-    
-  });
-}
+document.body.addEventListener('keydown', function (e) {
+  const key = e.key;
+	if (key === codes[index]){
+	  index++;
+
+	  if (index === codes.length) {
+	    alert("Hurray!");
+	    index = 0;
+	  }
+	} else {index = 0;
+	  } 
+} 
+)}
